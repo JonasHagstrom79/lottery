0525f63323fe8857b2c2eff869d0c3378c7d713d
JonasHagstrom79 <73818160+JonasHagstrom79@users.noreply.github.com>
2021-12-22 07:05:14 +0100

    deploy.js comments

--- deploy.js
+++ deploy.js
@@ -1,25 +1,31 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
-const Web3 = require('web3');
-const { interface, bytecode } = require('./compile');
+const Web3 = require('web3'); //web3 constructor
+const { interface, bytecode } = require('./compile'); //from our compile script
 
+//setup for HDWalletprovider
 const provider = new HDWalletProvider(
-  'REPLACE_WITH_YOUR_MNEMONIC',
-  // remember to change this to your own phrase!
-  'https://rinkeby.infura.io/v3/15c1d32581894b88a92d8d9e519e476c'
-  // remember to change this to your own endpoint!
+  'tennis make claw mad mosquito gorilla slab wide govern angry bomb turtle', //acount mnominic
+  'https://rinkeby.infura.io/v3/f4a45206e8ba4badada0e2f8197ce831' //link to the testnetwork
+  
 );
+//connection to the network
 const web3 = new Web3(provider);
 
+//to be able ro make 2 async calls
 const deploy = async () => {
+  //list of all unlocked accounts in the wallet
   const accounts = await web3.eth.getAccounts();
-
+  //address and public key that is deploying the contract
   console.log('Attempting to deploy from account', accounts[0]);
-
+  //Contract deployment statement
   const result = await new web3.eth.Contract(JSON.parse(interface))
+    //the bytecode and anyinitial arguments that we want to pass to the contract
     .deploy({ data: bytecode })
+    //sends the transaction to the network
     .send({ gas: '1000000', from: accounts[0] });
-
+    //shows the adress that our contract was deployed to
   console.log('Contract deployed to', result.options.address);
+  //Preventing a hanging deployment
   provider.engine.stop();
 };
 deploy();
